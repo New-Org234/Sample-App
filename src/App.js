acda9d153c1a7edeb180688f8c4aff0412427a49
Malavika1113 <74175454+Malavika1113@users.noreply.github.com>
2023-02-16 10:21:21 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          CPQ DATA NON PROD JOB.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
